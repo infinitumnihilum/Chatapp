9adc27c6e268797b325f687e7b389951c7e16e84
Joe Roddy <mrjoeroddy@gmail.com>
2016-02-17 19:03:31 -0500

    fix derp rng bug

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -105,7 +105,7 @@ angular.module('chat.controllers', [])
       var msgWords = msg.split(" ");
       msg="";
       for(var x=0;x<msgWords.length;x++){
-          var ran = Math.floor(Math.random()*9);
+          var ran = Math.floor(Math.random()*10);
           console.log("random element index: "+ran);
           var wordToAdd = derpWords[ran];
           msg+=wordToAdd+" ";
